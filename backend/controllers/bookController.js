f9b2ce96570854ffc6e5db229618a273f20d1859
Shartechs <shartechs@gmail.com>
2023-04-21 18:17:23 +0200

    added controllers

---
+++ backend/controllers/bookController.js
@@ -0,0 +1,70 @@
+const Book = require("../models/bookModel");
+const mongoose = require("mongoose");
+
+// Get all books
+const getBooks = async (req, res) => {
+  try {
+    const books = await Book.find().sort({ createdAt: -1 });
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Get a specific book
+const getBook = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ error: "No such book found" });
+
+  const book = await Book.findById(id);
+
+  if (!book) return res.status(400).json({ error: "No such book found" });
+  res.status(200).json(book);
+};
+// Add new book
+const addBook = async (req, res) => {
+  const { title, description, category, read } = req.body;
+
+  try {
+    const book = await Book.create({ title, description, category, read });
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Delete a book
+const deleteBook = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ error: "No such book found" });
+
+  const book = await Book.findOneAndDelete({ _id: id });
+
+  if (!book) return res.status(400).json({ error: "No such book found" });
+  res.status(200).json(book);
+};
+
+// Update a book
+const updateBook = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ error: "No such book found" });
+
+  const book = await Book.findOneAndUpdate({ _id: id }, { ...req.body });
+
+  if (!book) return res.status(400).json({ error: "No such book found" });
+  res.status(200).json(book);
+};
+
+module.exports = {
+  getBooks,
+  getBook,
+  addBook,
+  deleteBook,
+  updateBook,
+};
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,30 +1,20 @@
 const express = require("express");
-const Book = require("../models/BookModel");
+const {
+  getBooks,
+  getBook,
+  addBook,
+  deleteBook,
+  updateBook,
+} = require("../controllers/BookController");
 
 const router = express.Router();
 
 //Get all books
-router.get("/", (req, res) => {
-  res.json({ msg: "get all books" });
-});
+router.get("/", getBooks);
 // Get specific book
-router.get("/:id", (req, res) => {
-  res.json({ msg: "get single book" });
-});
-router.post("/", async (req, res) => {
-  const { title, description, category, read } = req.body;
-  try {
-    const book = Book.create({ title, description, category, read });
-    res.statusCode(200).json(book);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-router.delete("/:id", (req, res) => {
-  res.json({ msg: "delete a book" });
-});
-router.patch("/", (req, res) => {
-  res.json({ msg: "update a book" });
-});
+router.get("/:id", getBook);
+router.post("/", addBook);
+router.delete("/:id", deleteBook);
+router.patch("/:id", updateBook);
 
 module.exports = router;
