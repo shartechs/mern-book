8db0bc3f1fa82b6ff577c8d44310a0cde080abb1
Shartechs <shartechs@gmail.com>
2023-04-25 17:57:35 +0200

    added book deleting functionality

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { BooksContext } from "../contexts/BooksContext";
 
 const BookCard = ({ book }) => {
+  const { dispatch } = useContext(BooksContext);
+  const onDelete = async () => {
+    const response = await fetch(`/api/books/${book._id}`, {
+      method: "DELETE",
+    });
+    const deletedBook = await response.json();
+    try {
+      dispatch({ type: "DELETE_BOOK", payload: deletedBook });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="book-card">
       <h4>{book.title}</h4>
@@ -16,6 +30,7 @@ const BookCard = ({ book }) => {
         <strong>status: </strong>
         {book.status}
       </p>
+      <button onClick={onDelete}>Delete</button>
     </div>
   );
 };
--- frontend/src/contexts/BooksContext.js
+++ frontend/src/contexts/BooksContext.js
@@ -12,6 +12,10 @@ export const booksReducer = (state, action) => {
       return {
         books: [action.payload, ...state.books],
       };
+    case "DELETE_BOOK":
+      return {
+        books: state.books.filter((book) => book._id !== action.payload._id),
+      };
     default:
       return state;
   }
