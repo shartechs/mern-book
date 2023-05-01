8488c01fe3df2ff5852521498a6ef00668985380
Shartechs <shartechs@gmail.com>
2023-05-01 23:55:23 +0200

    added user model and controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,14 @@ const loginUser = async (req, res) => {
 };
 
 const signupUser = async (req, res) => {
-  res.json({ msg: "User signed up" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = { loginUser, signupUser };
