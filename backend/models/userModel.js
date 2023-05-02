ba83f37421397660b24d6b251106d706a31b65be
Shartechs <shartechs@gmail.com>
2023-05-02 18:31:08 +0200

    added validator logic to signup

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,6 +18,16 @@ const userSchema = new Schema({
 
 // statis signup method
 userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
+  }
+
   const exists = await this.findOne({ email });
 
   if (exists) {
