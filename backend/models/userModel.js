95f04ab2606d55854156d6191c86b3432ca4e179
shartechs <shartechs@gmail.com>
2023-05-02 22:54:41 +0400

    added login logic

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
   },
 });
 
-// statis signup method
+// static signup method
 userSchema.statics.signup = async function (email, password) {
   if (!email || !password) {
     throw Error("All fields must be filled");
@@ -38,6 +38,27 @@ userSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password");
+  }
 
   return user;
 };
